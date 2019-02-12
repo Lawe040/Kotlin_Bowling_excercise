14d4b1bb411832282ec7e69398d5599cfa9a111a
Lawrence Nortey <lawrence.inortey@gmail.com>
2019-02-12 01:07:00 +0100

    spare & strike logik kvar

--- app/src/main/java/com/example/getaccepttest/MainActivity.kt
+++ app/src/main/java/com/example/getaccepttest/MainActivity.kt
@@ -7,15 +7,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        allNumberButtons()
+        
+        numberButtonClicked()
 
         restart_button.setOnClickListener {
-
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -24,14 +22,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun allNumberButtons() {
+   private fun numberButtonClicked() {
 
-        val buttonNumbers = arrayOf(button_number_zero, button_number_one, button_number_two,button_number_three
-        , button_number_four, button_number_five, button_number_six, button_number_seven, button_number_eight
-        , button_number_nine, button_number_ten)
-        for (i in buttonNumbers.indices) {
+        val theNumberButtons = arrayOf(button_number_zero, button_number_one, button_number_two,
+            button_number_three, button_number_four, button_number_five, button_number_six, button_number_seven,
+            button_number_eight, button_number_nine, button_number_ten)
 
-            buttonNumbers[i].setOnClickListener {
+        for (i in theNumberButtons.indices) {
+
+            theNumberButtons[i].setOnClickListener {
                calculation(i)
             }
         }
@@ -39,71 +38,73 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculation(num:Int) {
 
-        val viewOne   = num.toString().toInt()
-        val viewTwo   = num.toString().toInt()
-        val viewThree = num.toString().toInt()
-        val viewFour  = num.toString().toInt()
-        val viewFive  = num.toString().toInt()
-        val viewSix   = num.toString().toInt()
-
         if (score_view_one.text.isEmpty()) {
 
-            score_view_one.text = "$viewOne"
+            score_view_one.text = "$num"
         }
+
         else if (score_view_two.text.isEmpty()) {
 
-            score_view_two.text = "$viewTwo"
+            score_view_two.text = "$num"
 
-            sumOfTheScores(inputOne = score_view_one.text.toString().toInt(),
-                inputTwo = score_view_two.text.toString().toInt())
+           sumOfTheScores(scoreInputOne = score_view_one.text.toString().toInt(),
+                scoreInputTwo = score_view_two.text.toString().toInt())
         }
+
         else if (score_view_three.text.isEmpty()) {
 
-          score_view_three.text = "$viewThree"
+          score_view_three.text = "$num"
         }
+
         else if (score_view_four.text.isEmpty()) {
 
-            score_view_four.text = "$viewFour"
+            score_view_four.text = "$num"
 
-             sumOfTheScores(inputOne = score_view_three.text.toString().toInt(),
-                inputTwo = score_view_four.text.toString().toInt())
+            sumOfTheScores(scoreInputOne = score_view_three.text.toString().toInt(),
+                scoreInputTwo = score_view_four.text.toString().toInt())
         }
+
         else if (score_view_five.text.isEmpty()) {
 
-            score_view_five.text = "$viewFive"
+            score_view_five.text = "$num"
         }
+
         else if (score_view_six.text.isEmpty()) {
 
-            score_view_six.text = "$viewSix"
+            score_view_six.text = "$num"
 
-            sumOfTheScores(inputOne = score_view_five.text.toString().toInt(),
-                inputTwo = score_view_six.text.toString().toInt())
+            sumOfTheScores(scoreInputOne = score_view_five.text.toString().toInt(),
+                scoreInputTwo = score_view_six.text.toString().toInt())
 
             high_score_value.text = current_score_value.text
         }
     }
 
-    private fun sumOfTheScores(inputOne: Int, inputTwo: Int){
+    private fun sumOfTheScores(scoreInputOne: Int, scoreInputTwo: Int){
 
-        val result = inputOne + inputTwo
+        val result = scoreInputOne + scoreInputTwo
 
         if (result_of_view_one.text.isEmpty()) {
 
             result_of_view_one.text = result.toString()
             current_score_value.text = result_of_view_one.text
         }
+
         else if (result_view_two.text.isEmpty()) {
 
             val secondResult = result_of_view_one.text.toString().toInt() + result
+
             result_view_two.text = secondResult.toString()
             current_score_value.text = result_view_two.text
         }
-        else  {
+
+        else {
 
             val thirdResult = result_view_two.text.toString().toInt() + result
+
             result_view_three.text = thirdResult.toString()
             current_score_value.text = result_view_three.text
         }
     }
-} // Class
+}
 
