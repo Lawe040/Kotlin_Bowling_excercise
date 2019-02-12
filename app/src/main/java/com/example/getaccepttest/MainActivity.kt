e47b8fc45818a401619aaf9214a5ca62678fc9a0
Lawrence Nortey <lawrence.inortey@gmail.com>
2019-02-12 16:07:36 +0100

    looks like a Java file ;) ... clean up next

--- app/src/main/java/com/example/getaccepttest/MainActivity.kt
+++ app/src/main/java/com/example/getaccepttest/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.getaccepttest
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    
+    private val scoreFullPoint = 20
+    private val scoreHalfPoint = 10
+    private val nullNumber     = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         numberButtonClicked()
+        strike_And_Spare_Result()
 
         restart_button.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -45,7 +52,7 @@ class MainActivity : AppCompatActivity() {
 
         else if (score_view_two.text.isEmpty()) {
 
-            score_view_two.text = "$num"
+           score_view_two.text = "$num"
 
            sumOfTheScores(scoreInputOne = score_view_one.text.toString().toInt(),
                 scoreInputTwo = score_view_two.text.toString().toInt())
@@ -84,15 +91,15 @@ class MainActivity : AppCompatActivity() {
 
         val result = scoreInputOne + scoreInputTwo
 
-        if (result_of_view_one.text.isEmpty()) {
+        if (result_view_one.text.isEmpty()) {
 
-            result_of_view_one.text = result.toString()
-            current_score_value.text = result_of_view_one.text
+            result_view_one.text = result.toString()
+            current_score_value.text = result_view_one.text
         }
 
         else if (result_view_two.text.isEmpty()) {
 
-            val secondResult = result_of_view_one.text.toString().toInt() + result
+            val secondResult = result_view_one.text.toString().toInt() + result
 
             result_view_two.text = secondResult.toString()
             current_score_value.text = result_view_two.text
@@ -104,6 +111,89 @@ class MainActivity : AppCompatActivity() {
 
             result_view_three.text = thirdResult.toString()
             current_score_value.text = result_view_three.text
+        }
+    }
+
+    private  fun strike_And_Spare_Result() {
+
+        strike_button.setOnClickListener {
+            
+            if (score_view_one.text.isEmpty() && score_view_two.text.isEmpty()) {
+
+                score_view_one.text        = nullNumber.toString()
+                score_view_two.text        = nullNumber.toString()
+                score_view_one.visibility  = View.GONE
+                score_view_two.visibility  = View.GONE
+                strike_view_one.visibility = View.VISIBLE
+
+                result_view_one.text       = "$scoreFullPoint"
+                current_score_value.text   = result_view_one.text
+            }
+
+            else if (score_view_one.text.isNotEmpty() && score_view_two.text.isEmpty()) {
+
+                score_view_two.text        = nullNumber.toString()
+                score_view_two.visibility  = View.GONE
+                strike_view_two.visibility = View.VISIBLE
+                strike_view_two.text       = "/"
+
+                result_view_one.text       = (score_view_one.text.toString().toInt() + scoreHalfPoint).toString()
+                current_score_value.text   = result_view_one.text
+            }
+
+            else if (score_view_two.text.isNotEmpty() && score_view_three.text.isEmpty()) {
+
+                score_view_three.text        = nullNumber.toString()
+                score_view_four.text         = nullNumber.toString()
+                score_view_three.visibility  = View.GONE
+                score_view_four.visibility   = View.GONE
+                strike_view_three.visibility = View.VISIBLE
+
+                result_view_two.text = ((result_view_one.text.toString().toInt() + scoreFullPoint).toString())
+                current_score_value.text = result_view_two.text
+
+            }
+
+            else if (score_view_three.text.isNotEmpty() && score_view_four.text.isEmpty()) {
+
+                score_view_four.text        = nullNumber.toString()
+                score_view_four.visibility  = View.GONE
+                strike_view_four.visibility = View.VISIBLE
+                strike_view_four.text       = "/"
+
+                result_view_two.text = ((result_view_one.text.toString().toInt() +
+                        score_view_three.text.toString().toInt() + scoreHalfPoint).toString())
+
+                current_score_value.text = result_view_two.text
+            }
+
+            else if (score_view_four.text.isNotEmpty() && score_view_five.text.isEmpty()) {
+
+                score_view_five.text        = nullNumber.toString()
+                score_view_six.text         = nullNumber.toString()
+                score_view_five.visibility  = View.GONE
+                score_view_six.visibility   = View.GONE
+                strike_view_five.visibility = View.VISIBLE
+
+                result_view_three.text = ((result_view_two.text.toString().toInt() + scoreFullPoint).toString())
+
+                current_score_value.text = result_view_three.text
+                high_score_value.text = result_view_three.text
+            }
+
+            else if (score_view_five.text.isNotEmpty() && score_view_six.text.isEmpty()) {
+
+                score_view_six.text        = nullNumber.toString()
+                score_view_six.visibility  = View.GONE
+                strike_view_six.visibility = View.VISIBLE
+                strike_view_six.text       = "/"
+
+                result_view_three.text = ((result_view_two.text.toString().toInt()
+                        + score_view_five.text.toString().toInt() + scoreHalfPoint).toString())
+
+                current_score_value.text = result_view_three.text
+                high_score_value.text = result_view_three.text
+            }
         }
     }
 }
